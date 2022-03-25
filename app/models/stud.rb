39711aa7e4c917990b6f9639f0c885e9e2ad84c7
abdur-rehman-ali <alijoyia911@gmail.com>
2022-03-25 22:53:26 +0500

    length,presence,numeriality

--- app/models/stud.rb
+++ app/models/stud.rb
@@ -1,2 +1,8 @@
 class Stud < ApplicationRecord
+    validates :name, presence:{message: 'Name to daloo'} 
+    validates :age, presence:{message: 'Age to daloo'} 
+
+    validates :name, length:{in: 2..6}
+
+    validates :age, numericality:{only_integer:true}
 end
