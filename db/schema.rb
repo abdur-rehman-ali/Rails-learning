ed5ece570f7410253377e0200a5f8a3fec77263f
abdur-rehman-ali <alijoyia911@gmail.com>
2022-03-26 22:40:12 +0500

    created models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_26_165920) do
+ActiveRecord::Schema.define(version: 2022_03_26_173854) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -24,6 +24,20 @@ ActiveRecord::Schema.define(version: 2022_03_26_165920) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
