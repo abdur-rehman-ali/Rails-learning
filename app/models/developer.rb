c25f2ca21a67f89416da00ca80b7b7e943dfd16f
abdur-rehman-ali <alijoyia911@gmail.com>
2022-03-27 21:27:36 +0500

    association made between models

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,2 +1,4 @@
 class Developer < ApplicationRecord
+    has_many :developer_projects
+    has_many :projects , through: :developer_projects
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,4 @@
 class Project < ApplicationRecord
+    has_many :developer_projects
+    has_many :developers , through: :developer_projects
 end
